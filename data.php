a1900888485c50bfaba07f533793ae1d88be1ed1
Maja Filipsson <majafilipsson@gmail.com>
2018-10-18 12:19:07 +0200

    Created 10 txt files w content

--- data.php
+++ data.php
@@ -8,6 +8,38 @@ $articles = [
     'pblsh-date' => '',
     'likes' => ''
   ],
+  [
+    'title' => '',
+    'content' => '',
+    'author' => '',
+    'pblsh-date' => '',
+    'likes' => ''
+  ],
+  [
+    'title' => '',
+    'content' => '',
+    'author' => '',
+    'pblsh-date' => '',
+    'likes' => ''
+  ],
+  [
+    'title' => '',
+    'content' => '',
+    'author' => '',
+    'pblsh-date' => '',
+    'likes' => ''
+  ],
+];
+
+$authors = [
+  [
+    'name' => '',
+    'id' => '',
+  ]
 ];
 
 print_r ($articles[0]);
+
+echo '<br>';
+
+print_r ($authors[0]);
