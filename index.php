3c0bca0ec071dff210f99ade4a8c0e5ef3a70bdf
Maja Filipsson <majafilipsson@gmail.com>
2018-10-26 22:28:49 +0200

    Improved styling

--- index.php
+++ index.php
@@ -10,26 +10,33 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/style.css">
-    <title>Fakey Flakey News Flow</title>
+    <title>Fake News</title>
   </head>
   <body>
 
-    <header>
-      <h1>Fake News</h1>
-      <nav class="nav">
-        <a href="index.php">Home</a>
-      </nav>
-    </header>
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-secondary text-light border-bottom shadow-sm">
+          <h4 class="my-0 mr-md-auto font-weight-normal">Fake News</h4>
+          <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-light" href="#">Features</a>
+            <a class="p-2 text-light" href="#">Enterprise</a>
+            <a class="p-2 text-light" href="#">Support</a>
+            <a class="p-2 text-light" href="#">Pricing</a>
+          </nav>
+        </div>
 
-    <div class="main">
+        <div class="shadow p-3 mb-4 mx-4 mt-4 rounded article">
+          <div class="bg-transparent clearfix">
+            <h5 class="float-left mb-0">Title</h5>
+            <p class="float-right mb-0">Author name</p>
+          </div>
+          <hr>
+          <p>Lots of text content here.</p>
+        </div>
 
-      <div class="article">
-        <h3>Title</h3>
-        <hr>
-      </div>
 
-    </div>
+
 
 
   </body>
