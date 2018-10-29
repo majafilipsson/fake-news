2aa2eb7fe03f0aec63e2ccdb404f6e41dd41c46f
Maja Filipsson <majafilipsson@gmail.com>
2018-10-29 17:08:47 +0100

    Changed some styling

--- articles.php
+++ articles.php
@@ -3,16 +3,19 @@ declare(strict_types=1);
 
 ?>
 
-<?php usort($articles, "compare"); ?>
-<?php foreach ($articles as $article) : ?>
-  <div class="shadow p-3 m-4 rounded article">
-    <div class="bg-transparent clearfix">
-      <h4 class="float-left mb-0"><?= $article['title'] ?></h4>
-      <p class="float-right mb-0"><?= getAuthor($authors, $article['author']) ?></p>
-    </div>
-    <hr>
-    <p class="font-italic"><?= 'Published on ' . getPblshDate($article['pblsh-date']) ?></p>
-    <p><?= substr($article['content'], 0, 200).'...'; ?></p>
-    <img class="inline-block" src="img/like.png"><small class="inline-block"><?= $article['likes'] . ' likes' ?></small>
-  </div>
-<?php endforeach; ?>
+
+<div class="article-container">
+    <?php usort($articles, "compare"); ?>
+    <?php foreach ($articles as $article) : ?>
+        <div class="shadow p-3 rounded article">
+            <div class="bg-transparent clearfix">
+                <h4 class="float-left mb-0"><?= $article['title'] ?></h4>
+                <p class="float-right mb-0"><?= getAuthor($authors, $article['author']) ?></p>
+            </div>
+            <hr>
+            <p class="font-italic"><?= 'Published on ' . getPblshDate($article['pblsh-date']) ?></p>
+            <p><?= $article['content']; ?></p>
+            <img class="inline-block" src="img/like.png"><small class="inline-block"><?= $article['likes'] . ' likes' ?></small>
+        </div>
+    <?php endforeach; ?>
+</div>
